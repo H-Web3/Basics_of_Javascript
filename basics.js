0dd269d29d3c9e2b4847fef95007d24f5a8ed363
H-Web3 <visualcrn.001@gmail.com>
2024-06-09 06:30:42 -0700

    learn creating int, string and object data type

---
+++ basics.js
@@ -0,0 +1,20 @@
+var x = 5;   //var is keywodrd to create a variable;
+var y = 6;
+var z = x + y; 
+console.log(z);  // to see the output
+
+//It's a string data type
+var firstName= "Huzaifa"
+console.log(firstName);
+
+/*---------------------*/
+
+//cerating an object data type which is a combination of int datatype and string datatype
+
+var car = {
+    color : "Red",
+    engineNum : 1234,
+    model : "BMW"
+};
+console.log(car.model);
+
